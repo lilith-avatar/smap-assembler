c92ac4f037a117260e93e9caffa81db35b921cbd
Stephen Zhou <stv1024@gmail.com>
2020-11-27 14:36:01 +0800

    在.bat文件里给路径加引号

--- 打散.bat
+++ 打散.bat
@@ -1,3 +1,3 @@
-:: 两个路径分别为 <smap路径> <工程文件夹路径>
-node Disassemble.js D:\Lilith\SurvivalIsland.smap D:\Lilith\SurvivalIsland
+:: 两个路径分别为 "<smap路径>" "<工程文件夹路径>"
+node Disassemble.js "D:\Lilith\SurvivalIsland.smap" "D:\Lilith\SurvivalIsland"
 pause
--- 聚合.bat
+++ 聚合.bat
@@ -1,3 +1,3 @@
-:: 两个路径分别为 <目标smap路径> <工程文件夹路径>
-node Assemble.js D:\Lilith\SurvivalIsland.smap D:\Lilith\SurvivalIsland
+:: 两个路径分别为 "<目标smap路径>" "<工程文件夹路径>"
+node Assemble.js "D:\Lilith\SurvivalIsland.smap" "D:\Lilith\SurvivalIsland"
 pause
